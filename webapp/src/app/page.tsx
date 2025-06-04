ef071b6b02ecb8df457d340df9c13dd72b0f0da3
George Abreu Jr. <george8794@gmail.com>
2025-06-04 00:10:17 +0000

    more copy fixes

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -112,7 +112,10 @@ function MainContent() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[40vh]">
       <div className="bg-white/80 dark:bg-zinc-900/80 rounded-xl shadow-xl p-10 w-full max-w-2xl mb-8 border border-zinc-200 dark:border-zinc-800">
-        <h1 className="text-4xl font-bold mb-4 text-red-700 drop-shadow">Find your Red Button</h1>
+        <h1 className="text-4xl font-bold mb-4 drop-shadow">
+          <span className="text-zinc-900 dark:text-zinc-100 lowercase">find your </span>
+          <span className="text-red-700 lowercase">red button</span>
+        </h1>
         <p className="text-lg mb-8 text-zinc-700 dark:text-zinc-200"><br/>
           Digital consulting & development for businesses, creators, and dreamers. <br /><br />
           Web, native, APIs, automation, SEO, marketing, and more.<br /><br />
